76597b274ea2f3b63c56152a0eedafa8ff76a602
vulcanobr <vulcanorjbr@gmail.com>
2024-03-25 18:25:36 -0300

    Creating the administrative dashboard home page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # get "welcome/index"
 
   namespace :administrate do
+    get "/" => "dashboards#index"
+
     resources :articles do
       member do
         delete :destroy_cover_image
