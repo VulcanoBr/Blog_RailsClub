110ff4a91bef4c4d65d6e9cb7fb35275bb37894a
vulcanobr <vulcanorjbr@gmail.com>
2024-05-21 16:17:25 -0300

    Adjusting author name and publication date on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@
 
 class WelcomeController < ApplicationController
   def index
-    @articles = Article.order(updated_at: :desc).limit(6)
+    @articles = Article.includes(:category, :author).order(updated_at: :desc).limit(6)
   end
 end
